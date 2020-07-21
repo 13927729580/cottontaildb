08ecfe22d57d28107536a7e34fa3c03046b06d25
gabuzi <gabriel.zihlmann@outlook.com>
2020-07-21 11:58:25 +0200

    more tests for investigating bit and bucket signatures

--- src/test/kotlin/org/vitrivr/cottontail/database/index/lsh/superbit/SuperBitLSHTest.kt
+++ src/test/kotlin/org/vitrivr/cottontail/database/index/lsh/superbit/SuperBitLSHTest.kt
@@ -1,22 +1,26 @@
 package org.vitrivr.cottontail.database.index.lsh.superbit
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.ValueSource
 import org.vitrivr.cottontail.math.knn.metrics.AbsoluteInnerProductDistance
-import org.vitrivr.cottontail.math.knn.metrics.CosineDistance
+import org.vitrivr.cottontail.math.knn.metrics.RealInnerProductDistance
+import org.vitrivr.cottontail.model.values.Complex64Value
 import org.vitrivr.cottontail.model.values.Complex64VectorValue
+import org.vitrivr.cottontail.model.values.DoubleVectorValue
+import org.vitrivr.cottontail.model.values.types.VectorValue
+import java.io.File
 import java.util.*
 import kotlin.time.ExperimentalTime
 
 internal class SuperBitLSHTest {
     companion object {
-        private val stages = arrayOf(1, 2, 4, 8)
-        private val buckets = arrayOf(32, 64, 128, 256, 512, 1024, 2048, 4096, 8192)
+        private val stages = arrayOf(8, 20)
+        private val buckets = arrayOf(4, 16, 32, 64, 128, 256, 512)
         private val seeds = arrayOf(1234L, 4321L, 82134L, 1337L, 42L)
+        private val numVectors = 100
 
         @JvmStatic
         fun provideConfigurationsForSBLSH(): List<Arguments> {
@@ -35,28 +39,83 @@ internal class SuperBitLSHTest {
     @MethodSource("provideConfigurationsForSBLSH")
     fun testRandomComplex64Vectors(stages: Int, buckets: Int, seed: Long) {
         println("stages: $stages buckets: $buckets seed: $seed")
-        val rng = SplittableRandom(seed)
+        val rng = Random(seed)
         val numDim = 20
         val lsh = SuperBitLSH(stages, buckets, numDim, seed, Complex64VectorValue.zero(numDim))
-        val vectors = Array(20) {
-            val v = Complex64VectorValue.random(numDim, rng)
+        val vectors = Array(numVectors) {
+            val v = Complex64VectorValue(DoubleArray(numDim * 2) { rng.nextGaussian() })
             v / v.norm2()
         }
+        compareNormalizedVectors(vectors as Array<VectorValue<*>>, lsh, File("testOut", "complex64/complexVectorsBucketDistances_stages${stages}buckets${buckets}seed${seed}.csv"))
+    }
+
+    private fun compareNormalizedVectors(vectors: Array<VectorValue<*>>, lsh: SuperBitLSH, outCSVFile: File) {
+        outCSVFile.parentFile.mkdirs()
         val bucketSignatures = vectors.map {
             lsh.hash(it)
         }
 
-        // compare pair-wise bucket signature and cosine distance
-        println("i,j,cosinedistance,numDiffBuckets")
-        for (i in vectors.indices) {
-            for (j in i until vectors.size) {
-                val a = vectors[i]
-                val b = vectors[j]
-                val d = AbsoluteInnerProductDistance(a, b)
-                val d2 = (bucketSignatures[i] zip bucketSignatures[j]).map { (a, b) ->
-                    if (a == b) 0 else 1
-                }.sum()
-                println("$i,$j,${d.value},${d2}")
+        // compare pair-wise bucket signature and IP distances
+        csvWriter().open(outCSVFile) {
+        writeRow(listOf("i", "j", "absoulteIPDistance", "realIPDistance", "numDiffBuckets"))
+            for (i in vectors.indices) {
+                for (j in i until vectors.size) {
+                    val a = vectors[i]
+                    val b = vectors[j]
+                    val d = AbsoluteInnerProductDistance(a, b)
+                    val r = RealInnerProductDistance(a, b)
+                    val d2 = (bucketSignatures[i] zip bucketSignatures[j]).map { (a, b) ->
+                        if (a == b) 0 else 1
+                    }.sum()
+                    writeRow(listOf(i, j, d.value, r.value, d2))
+                }
+            }
+        }
+    }
+
+    @ExperimentalTime
+    @ParameterizedTest
+    @MethodSource("provideConfigurationsForSBLSH")
+    fun testRandomDoubleVectors(stages: Int, buckets: Int, seed: Long) {
+        println("stages: $stages buckets: $buckets seed: $seed")
+        val rng = Random(seed)
+        val numDim = 20
+        val lsh = SuperBitLSH(stages, buckets, numDim, seed, DoubleVectorValue.zero(numDim))
+        val vectors = Array(numVectors) {
+            val v = DoubleVectorValue(DoubleArray(numDim) { rng.nextGaussian() })
+            v / v.norm2()
+        }
+        compareNormalizedVectors(vectors as Array<VectorValue<*>>, lsh, File("testOut", "real/realVectorsBucketDistances_stages${stages}buckets${buckets}seed${seed}.csv"))
+    }
+
+    @ExperimentalTime
+    @ParameterizedTest
+    @MethodSource("provideConfigurationsForSBLSH")
+    fun testComplexVectorsFromFile(stages: Int, buckets: Int, seed: Long) {
+        println("stages: $stages buckets: $buckets seed: $seed")
+        val numDim = 20
+        val lsh = SuperBitLSH(stages, buckets, numDim, seed, DoubleVectorValue.zero(numDim))
+        val file = File("src/test/resources/sampledVectors.csv")
+        if (!file.exists()) {
+            sampleVectorsFromCsv("src/test/resources/complexVectors.csv", "src/test/resources/sampledVectors.csv")
+        }
+        val vecs = csvReader().open(file) {
+            readAllAsSequence().drop(1).toList()
+        }
+        println("${vecs.size} vectors read.")
+        val vectors = vecs.map { row ->
+            val v = Complex64VectorValue((0 until numDim).map { Complex64Value(row[it + 1].toDouble(), row[it + 2].toDouble()) }.toTypedArray())
+            v / v.norm2()
+        }.toTypedArray()
+        compareNormalizedVectors(vectors as Array<VectorValue<*>>, lsh, File("testOut", "fromCsv/CsvVectorsBucketDistances_stages${stages}buckets${buckets}seed${seed}.csv"))
+    }
+
+    private fun sampleVectorsFromCsv(infile: String, outfile: String) {
+        val rng = Random()
+        csvReader().open(infile) {
+            csvWriter().open(outfile) {
+                writeRow(readNext()!!)
+                writeRows(readAllAsSequence().filter { rng.nextDouble() < 1e-5 }.toList())
             }
         }
     }
--- src/test/kotlin/org/vitrivr/cottontail/database/index/lsh/superbit/SuperBitTest.kt
+++ src/test/kotlin/org/vitrivr/cottontail/database/index/lsh/superbit/SuperBitTest.kt
@@ -1,14 +1,18 @@
 package org.vitrivr.cottontail.database.index.lsh.superbit
 
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.vitrivr.cottontail.math.basics.isApproximatelyTheSame
 import org.vitrivr.cottontail.math.knn.metrics.AbsoluteInnerProductDistance
 import org.vitrivr.cottontail.math.knn.metrics.RealInnerProductDistance
+import org.vitrivr.cottontail.model.values.Complex64Value
 import org.vitrivr.cottontail.model.values.Complex64VectorValue
 import org.vitrivr.cottontail.model.values.DoubleVectorValue
 import org.vitrivr.cottontail.model.values.types.VectorValue
+import java.io.File
 import java.util.*
 
 internal class SuperBitTest {
@@ -27,69 +31,73 @@ internal class SuperBitTest {
             }
         }
     }
+
     @ParameterizedTest
     @MethodSource("provideConfigurationsForSB")
     fun signatureComplex(N: Int, L: Int) {
         println("N: $N, L: $L")
-        val rng = SplittableRandom(1234)
+        val outDir = File("testOut/complex64")
+        val rng = Random(1234)
         val sb = SuperBit(numDim, N, L, 1234, Complex64VectorValue.zero(20))
-        // check orthogonality
-        for (i in sb.hyperplanes.indices) {
-            for (j in i until sb.hyperplanes.size) {
-                println("$i, $j: ${(sb.hyperplanes[i] dot sb.hyperplanes[j]).abs().value}")
-            }
-        }
         val vectors = Array(numVecs) {
-            val v = Complex64VectorValue.random(numDim, rng)
+            val v = Complex64VectorValue(DoubleArray(numDim * 2) { rng.nextGaussian() })
             v / v.norm2()
         }
-        val signatures = vectors.map {
-            sb.signature(it)
-        }
-
-        // compare pair-wise bucket signature and cosine distance
-        // we build a bucket-hamming distance on the basis of how many buckets are different... (probably shitty this way)
-        println("i,j,absoluteIPDist,realIPDist,hammingDist")
-        for (i in vectors.indices) {
-            for (j in i until vectors.size) {
-                val a = vectors[i]
-                val b = vectors[j]
-                val d = AbsoluteInnerProductDistance(a, b)
-                val dreal = RealInnerProductDistance(a, b)
-                val hd = (signatures[i] zip signatures[j]).map { (a, b) ->
-                    if (a == b) 0 else 1
-                }.sum()
-                println("$i,$j,${d.value},${dreal.value},${hd}")
-            }
-        }
+        compareNormalizedVectors(vectors as Array<VectorValue<*>>, sb, File(outDir, "complex64SBLSHSignaturesN${N}L${L}.csv"))
     }
+
+    @ParameterizedTest
+    @MethodSource("provideConfigurationsForSB")
+    fun signatureComplexFromFile(N: Int, L: Int) {
+        println("N: $N, L: $L")
+        val outDir = File("testOut/fromCsv")
+        val sb = SuperBit(numDim, N, L, 1234, Complex64VectorValue.zero(20))
+        val vecs = csvReader().open("src/test/resources/sampledVectors.csv") {
+            readAllAsSequence().drop(1).toList()
+        }
+        println("${vecs.size} vectors read.")
+        val vectors = vecs.map { row ->
+            val v = Complex64VectorValue((0 until numDim).map { Complex64Value(row[it + 1].toDouble(), row[it + 2].toDouble()) }.toTypedArray())
+            v / v.norm2()
+        }.toTypedArray()
+        compareNormalizedVectors(vectors as Array<VectorValue<*>>, sb, File(outDir, "complex64SBLSHSignaturesN${N}L${L}.csv"))
+    }
+
     @ParameterizedTest
     @MethodSource("provideConfigurationsForSB")
     fun signatureReal(N: Int, L: Int) {
+        val outDir = File("testOut/real")
         println("N: $N, L: $L")
-        val rng = SplittableRandom(1234)
+        val rng = Random(1234)
         val sb = SuperBit(numDim, N, L, 1234, DoubleVectorValue.zero(20))
         val vectors = Array(numVecs) {
-            val v = DoubleVectorValue.random(numDim, rng)
+            val v = DoubleVectorValue(DoubleArray(numDim) { rng.nextGaussian() })
             v / v.norm2()
         }
+        compareNormalizedVectors(vectors as Array<VectorValue<*>>, sb, File(outDir, "doubleSBLSHSignaturesN${N}L${L}.csv"))
+    }
+
+    private fun compareNormalizedVectors(vectors: Array<VectorValue<*>>, sb: SuperBit, outCsvFile: File) {
         val signatures = vectors.map {
             sb.signature(it)
         }
 
-        // compare pair-wise bucket signature and cosine distance
-        // we build a bucket-hamming distance on the basis of how many buckets are different... (probably shitty this way)
-        println("i,j,absoluteIPDist,realIPDist,hammingDist")
-        for (i in vectors.indices) {
-            for (j in i until vectors.size) {
-                val a = vectors[i]
-                val b = vectors[j]
-                val d = AbsoluteInnerProductDistance(a, b)
-                val dreal = RealInnerProductDistance(a, b)
-                val hd = (signatures[i] zip signatures[j]).map { (a, b) ->
-                    if (a == b) 0 else 1
-                }.sum()
-                println("$i,$j,${d.value},${dreal.value},${hd}")
+        outCsvFile.parentFile.mkdirs()
+        csvWriter().open(outCsvFile) {
+            val header = listOf("i", "j", "absoluteIPDist", "realIPDist", "hammingDist")
+            writeRow(header)
+            for (i in vectors.indices) {
+                for (j in i until vectors.size) {
+                    val a = vectors[i]
+                    val b = vectors[j]
+                    val d = AbsoluteInnerProductDistance(a, b)
+                    val dreal = RealInnerProductDistance(a, b)
+                    val hd = (signatures[i] zip signatures[j]).map { (a, b) ->
+                        if (a == b) 0 else 1
+                    }.sum()
+                    val data = listOf(i, j, d.value, dreal.value, hd)
+                    writeRow(data)
+                }
             }
         }
     }
@@ -115,4 +123,17 @@ internal class SuperBitTest {
             }
         }
     }
+    /* to plot the compareNormalizedVectors output:
+    In [81]: fig, axes = plt.subplots(9, 9, sharex=True)
+
+In [82]: for l in L:
+    ...:     for n in N:
+    ...:         pd.read_csv(f"complex64/complex64SBLSHSignaturesN{n}L{l}_includingImag.csv").plot("realIPDist", "hammingDist", ax=axes[n-1][l-1], kind="scatter", s=1)
+    ...:         axes[n-1][l-1].set_title(f"N={n}, L={l}")
+    ...:
+
+In [83]: plt.suptitle("Complex including imaginary part in signature")
+Out[83]: Text(0.5, 0.98, 'Complex including imaginary part in signature')
+
+     */
 }
